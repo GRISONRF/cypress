baa9115b81f13ebf246772779b3ce203321fc98d
Grison <Rafaela.Grison@cartus.com>
2024-04-22 14:46:01 -0500

    test using aliases

--- cypress/integration/sessions.spec.js
+++ cypress/integration/sessions.spec.js
@@ -1,22 +1,34 @@
 /// <reference types="cypress" />
 
 describe("Sessions page", () => {
-    it("should navigate to conference sessions page and view day filter buttons", () => {
-      cy.visit("/conference");
-      cy.get("h1").contains("View Sessions").click();
-      cy.url().should("include", "/sessions");
+  
+  // Run before each test in the describe block
+  beforeEach(() => {
+    cy.visit("/conference");
+    cy.get("h1").contains("View Sessions").click();
+    cy.url().should("include", "/sessions");
 
+    // Define aliases here
+    cy.get("[data-cy=AllSessions]").as("AllSessionsBtn")
+      cy.get("[data-cy=Wednesday]").as("WednesdayBtn")
+      cy.get("[data-cy=Thursday]").as("ThursdayBtn")
+      cy.get("[data-cy=Friday]").as("FridayBtn")
+
+  })
+    it("should navigate to conference sessions page and view day filter buttons", () => {
+      
       // validate that buttons to filter by day exists
-      cy.get("[data-cy=AllSessions]")
-      cy.get("[data-cy=Wednesday]")
-      cy.get("[data-cy=Thursday]")
-      cy.get("[data-cy=Friday]")
+      cy.get("@AllSessionsBtn")
+      cy.get("@WednesdayBtn")
+      cy.get("@ThursdayBtn")
+      cy.get("@FridayBtn")
 
     });
 
     it("should filter sessions and only display Wednesdar sessions when Wednesday button is clicked", () => {
-      cy.get("[data-cy=Wednesday]").click()
+      cy.get("@WednesdayBtn").click()
 
+      cy.get("[data-cy=day]").should("have.length", 21)
       cy.get("[data-cy=day]").contains("Wednesday").should("be.visible")
       cy.get("[data-cy=day]").contains("Thursday").should("not.exist")
       cy.get("[data-cy=day]").contains("Friday").should("not.exist")
@@ -24,7 +36,7 @@ describe("Sessions page", () => {
     })
 
     it("should filter sessions and only display Wednesdar sessions when Thursday button is clicked", () => {
-      cy.get("[data-cy=Thursday]").click()
+      cy.get("@ThursdayBtn").click()
 
       cy.get("[data-cy=day]").contains("Thursday").should("be.visible")
       cy.get("[data-cy=day]").contains("Wednesday").should("not.exist")
@@ -33,7 +45,7 @@ describe("Sessions page", () => {
     })
 
     it("should filter sessions and only display Wednesdar sessions when Friday button is clicked", () => {
-      cy.get("[data-cy=Friday]").click()
+      cy.get("@FridayBtn").click()
 
       cy.get("[data-cy=day]").contains("Friday").should("be.visible")
       cy.get("[data-cy=day]").contains("Thursday").should("not.exist")
